bd52248e1fecd0a524501e52a0fc5b1781abd1a1
heathmcgowan <heathjmcgowan@gmail.com>
2019-03-12 17:23:36 +1300

    re-enabling mailchimp js

--- js/main.js
+++ js/main.js
@@ -27,15 +27,15 @@ $(document).ready(function(){
 
 
 
-// // MailChimp
-// $(document).ready(function(){
-// 	  	(function($) {
-//    		window.fnames = new Array(); 
-// 	   	window.ftypes = new Array();
-// 	   	fnames[0]= 'EMAIL';ftypes[0]='email'; 
-// 	   	fnames[1]='FNAME';ftypes[1]='text';  
-// 	   	fnames[2]='LNAME';ftypes[2]='text';
-//   	}());
-// 	  	var $mcj = $.noConflict(true);	
-// });
+// MailChimp
+$(document).ready(function(){
+	  	(function($) {
+   		window.fnames = new Array(); 
+	   	window.ftypes = new Array();
+	   	fnames[0]= 'EMAIL';ftypes[0]='email'; 
+	   	fnames[1]='FNAME';ftypes[1]='text';  
+	   	fnames[2]='LNAME';ftypes[2]='text';
+  	}());
+	  	var $mcj = $.noConflict(true);	
+});
 
