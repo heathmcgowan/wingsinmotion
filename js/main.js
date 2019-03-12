416aaae4821f609ceb9a718bf5ade1feeeb494dc
heathmcgowan <heathjmcgowan@gmail.com>
2019-03-12 17:23:02 +1300

    tidied homepage and gallery, added 'to top' button

--- js/main.js
+++ js/main.js
@@ -15,6 +15,11 @@ $(document).ready(function(){
 		$('.mobile-nav').toggleClass('off-screen');
 	});
 
+	// To top
+	$('.to-top, .to-top-image').on("click",function(){
+	      $("HTML, BODY").animate({ scrollTop: 0 }, 500);
+	});
+
 });
 
 
@@ -22,15 +27,15 @@ $(document).ready(function(){
 
 
 
-// MailChimp
-$(document).ready(function(){
-	  	(function($) {
-   		window.fnames = new Array(); 
-	   	window.ftypes = new Array();
-	   	fnames[0]= 'EMAIL';ftypes[0]='email'; 
-	   	fnames[1]='FNAME';ftypes[1]='text';  
-	   	fnames[2]='LNAME';ftypes[2]='text';
-  	}());
-	  	var $mcj = $.noConflict(true);	
-});
+// // MailChimp
+// $(document).ready(function(){
+// 	  	(function($) {
+//    		window.fnames = new Array(); 
+// 	   	window.ftypes = new Array();
+// 	   	fnames[0]= 'EMAIL';ftypes[0]='email'; 
+// 	   	fnames[1]='FNAME';ftypes[1]='text';  
+// 	   	fnames[2]='LNAME';ftypes[2]='text';
+//   	}());
+// 	  	var $mcj = $.noConflict(true);	
+// });
 
